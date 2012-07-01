4b73862c6be1ae2222a36aceb12d2d96d8165858
adnan <alicica87@gmail.com>
2012-07-01 23:53:46 +0200

    Second commit

--- CVCExample/src/com/project/CVCClass.java
+++ CVCExample/src/com/project/CVCClass.java
@@ -4,6 +4,7 @@ public class CVCClass {
 
 	public static void main(String[] args) {
 		System.out.println("This project is used for Github testing.");
+		System.out.println("Added a new line of code");
 	}
 
 }
